496498f397682869841df42f0bea4201c9500e47
yume29 <srv123123@me.com>
2021-08-14 19:18:05 +0900

    add 404page

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+
+
+const NotFoundPage = () => (
+<Layout>
+<h1 style={{ padding: "20vh 0", textAlign: "center"}}>お探しのページは見つかりませんでした。</h1>   
+</Layout>
+
+
+)
+
+export default NotFoundPage
