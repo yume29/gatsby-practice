4248f77734a6aeeb6c370a72c45267f5ef0143c9
yume29 <srv123123@me.com>
2021-08-24 00:07:50 +0900

    create seo.js

---
+++ src/components/seo.js
@@ -0,0 +1,68 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
+
+
+const Seo = props => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          lang
+          description
+          siteUrl
+          fbappid
+          locale
+        }
+      }
+    }
+  `)
+
+  const title = props.pagetitle 
+  ? `${props.pagetitle} | ${data.site.siteMetadata.title}`
+  : data.site.siteMetadata.title
+
+  const description = props.pagedesc || data.site.siteMetadata.description
+
+  const url =props.pagepath
+  ? `${data.site.siteMetadata.siteUrl}${props.pagepath}`
+  : data.site.siteMetadata.siteUrl
+
+  const imgUrl = props.pageimg
+  ? `${data.site.siteMetadata.siteUrl}${props.pageimg}`
+  : `${data.site.siteMetadata.siteUrl}/thumb.jpg`
+
+  const imgw = props.pageimgw || 1280
+  const imgh = props.pageimgh || 640
+
+
+  return(
+    <Helmet>
+      <html lang={data.site.siteMetadata.lang} />
+      <title>{title}</title>
+      <meta name="description" content={description}  />
+
+      <link rel="canonical" href={url} />
+
+      <meta property="og:site_name" content={data.site.siteMetadata.title} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="go:locale" content={data.site.siteMetadata.locale} />
+      <meta property="fb:app_id" content={data.site.siteMetadata.fbappid} />
+
+      <meta property="og:image" content={imgUrl} />
+      <meta property="og:image:width" content={imgw} />
+      <meta property="og:image:height" content={imgh} />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      
+
+    </Helmet>
+  )
+
+}
+export default Seo
+
