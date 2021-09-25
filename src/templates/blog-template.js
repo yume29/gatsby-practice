91356bc0e30a4c2a03999e813d57193ec4bd8446
yume29 <srv123123@me.com>
2021-09-25 22:39:57 +0900

    add blog-template.js

---
+++ src/templates/blog-template.js
@@ -0,0 +1,83 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons"
+
+
+const BlogPage = ({ data, location, pageContext }) => (
+  <Layout>
+  <Seo 
+    pagetitle="ブログ"
+    pagedesc="ESSENTIALのブログです"
+    pagepath={location.pathname}/>
+    <section className="content bloglist">
+      <div className="container">
+        <h1 className="bar">RECENT POSTS</h1>
+        <div className="posts">
+        {data.allContentfulBlogPost.edges.map(({node}) => (
+          <article className="post" key={node.id}>
+            <Link to={`/blog/post/${node.slug}`}>
+              <figure>
+                <GatsbyImage image={node.eyecatch.gatsbyImageData} alt={node.eyecatch.description} style={{ height: "100%" }}/>
+              </figure>
+              <h3>{node.title}</h3>
+            </Link>
+          </article>
+          ))}
+        </div>
+        <ul class="pagenation">
+          {!pageContext.isFirst && (
+            <li class="prev">
+                <Link to={
+                  pageContext.currentPage === 2
+                  ? `/blog/`
+                  : `/blog/${pageContext.currentPage - 1}/`
+                }
+                rel="prev"
+                >
+                    <FontAwesomeIcon icon={faChevronLeft}/>
+                    <span>前のページ</span>
+                </Link>
+            </li>
+            )}
+            {!pageContext.isLast && (
+            <li class="next">
+              <Link to={`/blog/${pageContext.currentPage + 1}/`} rel="next">
+                    <span>次のページ</span>
+                    <FontAwesomeIcon icon={faChevronRight}/>
+              </Link>
+            </li>
+            )}
+        </ul>
+
+      </div>
+    </section>
+  </Layout>
+)
+
+export const query = graphql`
+query($skip: Int!, $limit: Int!){
+  allContentfulBlogPost(
+    sort: {order: DESC, fields: publishDate}
+    limit: $limit
+    skip: $skip
+  ) {
+    edges {
+      node {
+        title
+        id
+        slug
+        eyecatch {
+          gatsbyImageData(layout: CONSTRAINED, width: 500)
+          description
+        }
+      }
+    }
+  }
+}
+`
+
+export default BlogPage
