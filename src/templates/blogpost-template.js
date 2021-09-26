9eb3147b926a93f205329ed59a6ee8ce573e75b3
yume29 <srv123123@me.com>
2021-09-26 18:13:27 +0900

    add links to access category page

--- src/templates/blogpost-template.js
+++ src/templates/blogpost-template.js
@@ -63,7 +63,7 @@ const BlogPost = ({ data, pageContext,location }) => (
             <ul>
               {data.contentfulBlogPost.category.map(cat => (
                 <li className={cat.categorySlug} key={cat.id}>
-                  {cat.category}
+                  <Link to={`/cat/${cat.categorySlug}/`}>{cat.category}</Link>
                 </li>
               ))}
             </ul>
